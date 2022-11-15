7bf42d27f7b744ad03e69464581d0bc625a2362a
buba <nodata@mail.com>
2022-11-15 22:34:45 +0600

    adress autocomplete raw

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -150,6 +150,7 @@
 <script src="{{ asset('js/jquery-1.11.0.min.js') }}"></script>
 <script src="{{ asset('js/plugins.js') }}"></script>
 <script src="{{ asset('js/modernizr.js') }}"></script>
+        @stack('scripts')
 <script src="{{ asset('js/script.js') }}"></script>
 </body>
 </html>
