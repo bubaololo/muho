4358b97c67216f9b8b8be7576595df411118567e
buba <nodata@mail.com>
2022-11-19 02:03:34 +0600

    checkout tablex, logo fine tuning

---
+++ database/migrations/2022_11_17_170000_create_credentials_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('credentials', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->nullable()
+                ->constrained()
+                ->onUpdate('cascade');
+            $table->tinyText('name');
+            $table->tinyText('surname');
+            $table->tinyText('middle_name');
+            $table->text('address');
+            $table->text('apartment')->nullable();
+            $table->text('last_ip')->nullable();
+            $table->text('comment')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        Schema::dropIfExists('credentials');
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+    }
+};
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,9 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Nunito:wght@400;600&family=Poppins:ital@1&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat&family=Nunito:wght@400;600&family=Poppins:ital@1&display=swap"
+        rel="stylesheet">
     <link href="{{ asset('css/normalize.css') }}" rel="stylesheet">
     <link href="{{ asset('css/icomoon.css') }}" rel="stylesheet">
     <link href="{{ asset('css/vendor.css') }}" rel="stylesheet">
@@ -29,130 +31,132 @@
 </head>
 <body>
 
-        <header id="header">
-            <div class="container">
-                <div class="row">
+<header id="header">
+    <div class="container">
+        <div class="row">
 
-                    <div class="col-md-2">
-                        <a href="{{ route('index') }}" class="main-logo">
-                            <img class="main-logo__part main-logo__part_base" src="{{ asset('images/tmpimg/logo_main_part.svg') }}" alt="logo">
-{{--                            <div class="main-logo__part main-logo__part_left-eye"></div>--}}
-                            <svg class="main-logo__part main-logo__part_left-eye" viewBox="0 0 12 12" version="1.1"
-                                 xmlns="http://www.w3.org/2000/svg">
-                                <circle cx="6" cy="6" r="5" fill="#FA370F"/>
-                            </svg>                            <svg class="main-logo__part main-logo__part_right-eye" viewBox="0 0 12 12" version="1.1"
-                                 xmlns="http://www.w3.org/2000/svg">
-                                <circle cx="6" cy="6" r="5" fill="#FA370F"/>
-                            </svg>
-                        </a>
+            <div class="col-md-2">
+                <a href="{{ route('index') }}" class="main-logo">
+                    <img class="main-logo__part main-logo__part_base"
+                         src="{{ asset('images/tmpimg/logo_main_part.svg') }}" alt="logo">
+                    {{--                            <div class="main-logo__part main-logo__part_left-eye"></div>--}}
+                    <svg class="main-logo__part main-logo__part-eye  main-logo__part_left-eye" viewBox="0 0 12 12" version="1.1"
+                         xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="6" cy="6" r="5" fill="#FA370F"/>
+                    </svg>
+                    <svg class="main-logo__part main-logo__part-eye main-logo__part_right-eye" viewBox="0 0 12 12" version="1.1"
+                         xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="6" cy="6" r="5" fill="#FA370F"/>
+                    </svg>
+                </a>
 
-                    </div>
+            </div>
 
-                    <div class="col-md-10">
+            <div class="col-md-10">
 
-                        <nav id="navbar">
-                            <div class="main-menu stellarnav">
-                                <ul class="menu-list">
-{{--                                    <li class="menu-item active"><a href="{{ route('index') }}"--}}
-{{--                                                                    data-effect="Home">Главная</a>--}}
-{{--                                    </li>--}}
-                                    <li class="menu-item"><a href="{{ route('products.list') }}" class="nav-link"
-                                                             data-effect="About">Товары</a>
-                                    </li>
-{{--                                    <li class="menu-item has-sub">--}}
-{{--                                        <a href="#pages" class="nav-link" data-effect="Pages">Pages</a>--}}
+                <nav id="navbar">
+                    <div class="main-menu stellarnav">
+                        <ul class="menu-list">
+                            {{--                                    <li class="menu-item active"><a href="{{ route('index') }}"--}}
+                            {{--                                                                    data-effect="Home">Главная</a>--}}
+                            {{--                                    </li>--}}
+                            <li class="menu-item"><a href="{{ route('products.list') }}" class="nav-link"
+                                                     data-effect="About">Товары</a>
+                            </li>
+                            {{--                                    <li class="menu-item has-sub">--}}
+                            {{--                                        <a href="#pages" class="nav-link" data-effect="Pages">Pages</a>--}}
 
-{{--                                        <ul>--}}
-{{--                                            <li class="active"><a href="{{ route('index') }}">Главная</a></li>--}}
-{{--                                            <li><a href="about.html">About</a></li>--}}
-{{--                                            <li><a href="styles.html">Styles</a></li>--}}
-{{--                                            <li><a href="blog.html">Blog</a></li>--}}
-{{--                                            <li><a href="single-post.html">Post Single</a></li>--}}
-{{--                                            <li><a href="{{ route('products.list') }}">Витрина</a></li>--}}
-{{--                                            <li><a href="single-product.html">Product Single</a></li>--}}
-{{--                                            <li><a href="contact.html">Contact</a></li>--}}
-{{--                                            <li><a href="thank-you.html">Thank You</a></li>--}}
-{{--                                        </ul>--}}
+                            {{--                                        <ul>--}}
+                            {{--                                            <li class="active"><a href="{{ route('index') }}">Главная</a></li>--}}
+                            {{--                                            <li><a href="about.html">About</a></li>--}}
+                            {{--                                            <li><a href="styles.html">Styles</a></li>--}}
+                            {{--                                            <li><a href="blog.html">Blog</a></li>--}}
+                            {{--                                            <li><a href="single-post.html">Post Single</a></li>--}}
+                            {{--                                            <li><a href="{{ route('products.list') }}">Витрина</a></li>--}}
+                            {{--                                            <li><a href="single-product.html">Product Single</a></li>--}}
+                            {{--                                            <li><a href="contact.html">Contact</a></li>--}}
+                            {{--                                            <li><a href="thank-you.html">Thank You</a></li>--}}
+                            {{--                                        </ul>--}}
 
-{{--                                    </li>--}}
-{{--                                    <li class="menu-item"><a href="#popular-books" class="nav-link"--}}
-{{--                                                             data-effect="Shop">Shop</a></li>--}}
-{{--                                    <li class="menu-item"><a href="#latest-blog" class="nav-link"--}}
-{{--                                                             data-effect="Articles">Articles</a>--}}
-{{--                                    </li>--}}
-                                    <li class="menu-item"><a href="#contact" class="nav-link"
-                                                             data-effect="Contact">Контакты</a></li>
-                                    <a href="{{ route('home') }}" class="user-account for-buy"><i
-                                            class="icon icon-user"></i><span>Аккаунт</span></a>
-                                    <a href="{{ route('cart.list') }}" class="cart for-buy"><i
-                                            class="icon icon-clipboard"></i><span>Корзина:({{ Cart::getTotalQuantity()}} товаров)</span></a>
-                                    <div class="action-menu">
+                            {{--                                    </li>--}}
+                            {{--                                    <li class="menu-item"><a href="#popular-books" class="nav-link"--}}
+                            {{--                                                             data-effect="Shop">Shop</a></li>--}}
+                            {{--                                    <li class="menu-item"><a href="#latest-blog" class="nav-link"--}}
+                            {{--                                                             data-effect="Articles">Articles</a>--}}
+                            {{--                                    </li>--}}
+                            <li class="menu-item"><a href="#contact" class="nav-link"
+                                                     data-effect="Contact">Контакты</a></li>
+                            <a href="{{ route('home') }}" class="user-account for-buy"><i
+                                    class="icon icon-user"></i><span>Аккаунт</span></a>
+                            <a href="{{ route('cart.list') }}" class="cart for-buy"><i
+                                    class="icon icon-clipboard"></i><span>Корзина:({{ Cart::getTotalQuantity()}} товаров)</span></a>
+                            <div class="action-menu">
 
-                                        <!-- Authentication Links -->
-                                        @guest
-                                            @if (Route::has('login'))
+                                <!-- Authentication Links -->
+                                @guest
+                                    @if (Route::has('login'))
 
-                                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
 
-                                            @endif
+                                    @endif
 
-                                            @if (Route::has('register'))
+                                    @if (Route::has('register'))
 
-                                                <a class="nav-link"
-                                                   href="{{ route('register') }}">{{ __('Register') }}</a>
+                                        <a class="nav-link"
+                                           href="{{ route('register') }}">{{ __('Register') }}</a>
 
-                                            @endif
-                                        @else
-                                            <li class="nav-item dropdown">
-                                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#"
-                                                   role="button" data-bs-toggle="dropdown" aria-haspopup="true"
-                                                   aria-expanded="false" v-pre>
-                                                    {{ Auth::user()->name }}
-                                                </a>
+                                    @endif
+                                @else
+                                    <li class="nav-item dropdown">
+                                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#"
+                                           role="button" data-bs-toggle="dropdown" aria-haspopup="true"
+                                           aria-expanded="false" v-pre>
+                                            {{ Auth::user()->name }}
+                                        </a>
 
-                                                <div class="dropdown-menu dropdown-menu-end"
-                                                     aria-labelledby="navbarDropdown">
-                                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                                       onclick="event.preventDefault();
+                                        <div class="dropdown-menu dropdown-menu-end"
+                                             aria-labelledby="navbarDropdown">
+                                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                               onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                                        {{ __('Logout') }}
-                                                    </a>
+                                                {{ __('Logout') }}
+                                            </a>
 
-                                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                                          class="d-none">
-                                                        @csrf
-                                                    </form>
-                                                </div>
-                                            </li>
-                                        @endguest
-                                        <!--top-right-->
-
-                                    </div>
-                                </ul>
-
-                                <div class="hamburger">
-                                    <span class="bar"></span>
-                                    <span class="bar"></span>
-                                    <span class="bar"></span>
-                                </div>
+                                            <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                                  class="d-none">
+                                                @csrf
+                                            </form>
+                                        </div>
+                                    </li>
+                                @endguest
+                                <!--top-right-->
 
                             </div>
-                        </nav>
+                        </ul>
+
+                        <div class="hamburger">
+                            <span class="bar"></span>
+                            <span class="bar"></span>
+                            <span class="bar"></span>
+                        </div>
 
                     </div>
+                </nav>
 
-                </div>
             </div>
-        </header>
-    </div><!--header-wrap-->
 
-        @yield('content')
+        </div>
+    </div>
+</header>
+</div><!--header-wrap-->
 
-        @vite(['resources/js/app.js']);
+@yield('content')
+
+@vite(['resources/js/app.js']);
 <script src="{{ asset('js/jquery-1.11.0.min.js') }}"></script>
 <script src="{{ asset('js/plugins.js') }}"></script>
 <script src="{{ asset('js/modernizr.js') }}"></script>
-        @stack('scripts')
+@stack('scripts')
 <script src="{{ asset('js/script.js') }}"></script>
 </body>
 </html>
