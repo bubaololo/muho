b5922b6d4af065c0b33a2d4b776ed9fc1ca8a58b
bubaololo <bubaololo@gmail.com>
2023-01-08 02:20:14 +0600

    top padding of content fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -154,9 +154,9 @@
     </div>
 </header>
 </div><!--header-wrap-->
-
+<div class="content-wrapper">
 @yield('content')
-
+</div>
 <footer class="footer">
     <a href="{{ route('cart.list') }}" class="cart mobile-cart">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
