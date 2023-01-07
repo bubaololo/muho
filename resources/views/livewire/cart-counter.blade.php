040d0735fc6af38b32ea79364b951edf33230709
bubaololo <bubaololo@gmail.com>
2023-01-08 02:09:35 +0600

    mibile cart icon implemented

--- resources/views/livewire/cart-counter.blade.php
+++ resources/views/livewire/cart-counter.blade.php
@@ -1,11 +1,11 @@
 @if ($cart_count == 0)
-    <span>пусто</span>
+    <span class="cart__text" >пусто</span>
 @elseif ($cart_count == 1)
-    <span> <strong class="cart-accent">{{ $cart_count }}</strong> товар</span>
+    <span> <strong class="cart-accent">{{ $cart_count }}</strong> <span class="cart__text"> товар</span></span>
 @elseif  ($cart_count > 1 && $cart_count < 5)
-    <span><strong class="cart-accent" >{{ $cart_count }}</strong> товара</span>
+    <span><strong class="cart-accent" >{{ $cart_count }}</strong> <span class="cart__text"> товара</span></span>
 @else
-    <span><strong class="cart-accent" >{{ $cart_count }}</strong> товаров</span>
+    <span><strong class="cart-accent" >{{ $cart_count }}</strong><span class="cart__text"> товаров</span></span>
 @endif
 
 
